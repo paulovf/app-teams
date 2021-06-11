7f5341214df71c9d9f74db7fa45f37a6739dd922
paulovf <paulovfrancisco@gmail.com>
2021-06-11 09:29:45 -0300

    alterado: arquivo package.jsom

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -14,7 +14,6 @@ module.exports = webpackMerge(commonConfig(), {
     devServer: {
         https: true,
         host: "0.0.0.0",
-        port: process.env.PORT || 3000,
         historyApiFallback: true,
         stats: 'minimal',
         watchOptions: {
